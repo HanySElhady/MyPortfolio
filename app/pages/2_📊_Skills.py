84838ae5b5872c56704682c40876a4e7f86c0101
HanySElhady <46346727+HanySElhady@users.noreply.github.com>
2025-11-20 23:03:33 +0200

    Update 2_📊_Skills.py

--- app/pages/2_📊_Skills.py
+++ app/pages/2_📊_Skills.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import json
 
-config = json.load(open("config.json", "r", encoding="utf-8"))
+config = json.load(open("app/config.json", "r", encoding="utf-8"))
 
 st.title("📊 Skills")
 
 for skill in config["skills"]:
     st.markdown(f"- ✅ **{skill}**")
+
