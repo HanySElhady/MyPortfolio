ab0534b2858189c201bc3fab5264cca50ed9ad5c
HanySElhady <46346727+HanySElhady@users.noreply.github.com>
2025-11-20 22:47:18 +0200

    Update 1_🏠_Home.py

--- app/pages/1_🏠_Home.py
+++ app/pages/1_🏠_Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json
 from PIL import Image
 
-config = json.load(open("config.json", "r", encoding="utf-8"))
+config = json.load(open("app/config.json", "r", encoding="utf-8"))
 
 #st.title("🏠 الصفحة الرئيسية")
 st.write(config["about_me"])
@@ -13,4 +13,5 @@ st.image("assets/certificates/cert1.png", width=400)
 st.image("assets/certificates/cert2.png", width=400)
 st.image("assets/certificates/cert3.png", width=400)
 st.image("assets/certificates/cert4.png", width=400)
+
 st.image("assets/certificates/cert5.png", width=400)
