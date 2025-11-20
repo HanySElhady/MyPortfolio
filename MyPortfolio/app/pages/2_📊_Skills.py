3a4e729301c9fba6548e39f726dd64833aacbe85
HanySElhady <46346727+HanySElhady@users.noreply.github.com>
2025-11-20 22:21:25 +0200

    Add files via upload

---
+++ MyPortfolio/app/pages/2_📊_Skills.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import json
+
+config = json.load(open("config.json", "r", encoding="utf-8"))
+
+st.title("📊 Skills")
+
+for skill in config["skills"]:
+    st.markdown(f"- ✅ **{skill}**")
---
+++ MyPortfolio/app/pages/4_📬_Contact.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import json
+
+config = json.load(open("config.json", "r", encoding="utf-8"))
+
+st.title("📬 Contact me")
+
+st.write("### 📧 Email")
+st.write(config["email"])
+
+st.write("### 📧 Linkedin")
+st.write(config["linkedin"])
+
+st.write("### 📞 Phone")
+st.write(config["phone"])
+
+st.write("### 📍 Location")
+st.write(config["location"])
